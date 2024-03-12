50ee7bb2f09dceac7651a6daf036de8c4cbaa576
bparks13 <bap@open-ephys.org>
2024-03-12 15:52:11 -0400

    Add TCP Node
    Sends an OpenCV.Mat variable over a TCP connection

---
+++ OpenEphys.Bonsai.EphysSocket/OpenEphys.Bonsai.EphysSocket/OpenCVMatTCPClient.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Reactive.Linq;
+using System.Runtime.InteropServices;
+using Bonsai;
+using OpenCV.Net;
+
+namespace OpenEphys.Bonsai.EphysSocket
+{
+    [Combinator]
+    [WorkflowElementCategory(ElementCategory.Sink)]
+    [Description("Sends a 2D Open CV Mat to a TCP Socket. Element type is preserved.")]
+    public class OpenCVMatTCPClient : Sink<Mat>
+    {
+        [Description("Address")]
+        public string Address { get; set; } = "localhost";
+
+        [Description("Port number. Changing while running has no effect.")]
+        public int Port { get; set; } = 5000;
+
+        public override IObservable<Mat> Process(IObservable<Mat> source)
+        {
+            return Observable.Using(
+                () =>
+                {
+                    if (Address == "localhost")
+                    {
+                        Address = "127.0.0.1";
+                    }
+
+                    var listener = new TcpListener(IPAddress.Parse(Address), Port);
+                    listener.Start();
+
+                    TcpClient client = listener.AcceptTcpClient();
+                    NetworkStream stream = client.GetStream();
+                    return stream;
+                },
+                stream =>
+                {
+                    return source.Do(value =>
+                    {
+                        const int headerSize = 22;
+
+                        int numBytes = value.ElementSize * value.Cols * value.Rows;
+                        byte[] numBytesBytes = BitConverter.GetBytes(numBytes);
+
+                        int offset = 0;
+                        byte[] offsetBytes = BitConverter.GetBytes(offset);
+
+                        byte[] bitDepthBytes = BitConverter.GetBytes((short)value.Depth);
+                        byte[] elementSizeBytes = BitConverter.GetBytes(value.ElementSize);
+                        byte[] numChannelsBytes = BitConverter.GetBytes(value.Rows);
+                        byte[] numSamplesBytes = BitConverter.GetBytes(value.Cols);
+
+                        var data = new byte[numBytes + headerSize];
+
+                        Buffer.BlockCopy(offsetBytes, 0, data, 0, 4);
+                        Buffer.BlockCopy(numBytesBytes, 0, data, 4, 4);
+                        Buffer.BlockCopy(bitDepthBytes, 0, data, 8, 2);
+                        Buffer.BlockCopy(elementSizeBytes, 0, data, 10, 4);
+                        Buffer.BlockCopy(numChannelsBytes, 0, data, 14, 4);
+                        Buffer.BlockCopy(numSamplesBytes, 0, data, 18, 4);
+
+                        Marshal.Copy(value.Data + offset, data, headerSize, numBytes);
+
+                        try
+                        {
+                            stream.Write(data, 0, data.Length);
+                        }
+                        catch (IOException) // NB: Socket was closed
+                        {
+                            var listener = new TcpListener(IPAddress.Parse(Address), Port);
+                            listener.Start();
+                            TcpClient client = listener.AcceptTcpClient();
+                            stream = client.GetStream();
+                        }
+                    });
+                });
+        }
+    }
+}
